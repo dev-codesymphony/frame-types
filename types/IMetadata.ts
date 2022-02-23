4e01f9119a06e7b8cc8c5cba48d399e7be3ec26e
Neel Patel <neelpatel.6531@gmail.com>
2022-02-23 16:30:20 +0530

    add metadata type

---
+++ types/IMetadata.ts
@@ -0,0 +1,15 @@
+export interface IAttributes {
+  trait_type: string;
+  value: string;
+}
+
+export interface IMetadata {
+  description: string;
+  image: string;
+  dna: string;
+  edition: string;
+  attributes: IAttributes[];
+  compiler: string;
+  filename: string;
+  filedate: number
+}
