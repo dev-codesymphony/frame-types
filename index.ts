e19ad60b7d33bed0c476900daeac22d532dafa07
Neel Patel <neelpatel.6531@gmail.com>
2022-02-23 16:31:48 +0530

    add metadata type

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export type { ILayer, IOrderLayers } from "./types/IOrderLayers";
 export type { IRules, RuleObj } from "./types/IRules";
+export type { IAttributes, IMetadata } from "./types/IMetadata"
